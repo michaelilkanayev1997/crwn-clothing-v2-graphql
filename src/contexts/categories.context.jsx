55e8a87c1e47f2873d3f5fb1450033e8ff845862
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-04 22:58:14 +0300

    Categories with GraphQl Queries

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -5,8 +5,27 @@ export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
+
+  console.log("loading", loading);
+  console.log("data", data);
 
   const value = { categoriesMap };
   return (
